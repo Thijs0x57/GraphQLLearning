2c5e7075ef5a86c8f622cea82dc2d329be871f04
Thijs Wijnen <t.wijnen@gdksoftware.nl>
2019-04-11 11:01:25 +0100

    Added mutation

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+const database_uri="mongodb+srv://thijs:test1234@graphqllearning-aeso0.mongodb.net/test?retryWrites=true"
+mongoose.connect(database_uri,{
+    useNewUrlParser: true
+});
+mongoose.connection.once('open', ()=> {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
