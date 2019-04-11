f03eb7ad3717b4aaf1e8628fb9dbac5d6a7edd56
Thijs Wijnen <t.wijnen@gdksoftware.nl>
2019-04-11 16:37:34 +0100

    Added book detail function

--- frontend/client/src/components/BookList.js
+++ frontend/client/src/components/BookList.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: null
+        }
+    }
     displayBooks() {
         var data = this.props.data;
         if (!data.loading) {
             return data.books.map(book => {
                 return (
-                    <li key={book.id}>
+                    <li key={book.id} onClick={(e) => { this.setState({ selected: book.id }) }}>
                         {book.name}
                     </li>
                 );
@@ -24,6 +31,7 @@ class BookList extends Component {
                 <ul id="book-list">
                     {this.displayBooks()}
                 </ul>
+                <BookDetails bookId={this.state.selected} />
             </div>
         );
     }
